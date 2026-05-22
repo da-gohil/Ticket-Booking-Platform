283f3fb5dae1920a14ac863163fe4230cd41ff19
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-05-22 16:39:07 -0400

    Create CreateEventRequest.java

---
+++ src/main/java/com/danny/ticket/domain/CreateEventRequest.java
@@ -0,0 +1,29 @@
+package com.danny.ticket.domain;
+
+import com.danny.ticket.domain.entities.EventStatusEnum;
+import com.danny.ticket.domain.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequest {
+
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
+
+
+}

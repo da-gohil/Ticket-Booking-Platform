4aae982599a48da7107bfdd73733b2efb52aae72
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-05-26 22:57:33 -0400

    Create Security.java

---
+++ src/main/java/com/danny/ticket/config/Security.java
@@ -0,0 +1,33 @@
+package com.danny.ticket.config;
+
+import com.danny.ticket.filters.UserProvisionFilter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Security {
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            UserProvisionFilter userProvisionFilter) throws Exception{
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated())
+                .csrf(csrf ->  csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(
+                                Customizer.withDefaults()
+                        ))
+                .addFilterAfter(userProvisionFilter, BearerTokenAuthenticationFilter.class);
+        return http.build();
+    }
+}

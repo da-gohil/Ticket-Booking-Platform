5698f693132dd990c2e2eaa4cc48900875c68255
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-01 12:00:36 -0400

    Create CreateEventRequestDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/CreateEventRequestDTO.java
@@ -0,0 +1,27 @@
+package com.danny.ticket.domain.dtos;
+
+import com.danny.ticket.domain.CreateTicketTypeRequest;
+import com.danny.ticket.domain.entities.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequestDTO {
+
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
+
+}

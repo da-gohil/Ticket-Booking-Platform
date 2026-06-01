6268e840c5f939f3fd028cf3b5fcb593552cc803
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-01 15:43:43 -0400

    Update CreateEventRequestDTO.java

--- src/main/java/com/danny/ticket/domain/dtos/CreateEventRequestDTO.java
+++ src/main/java/com/danny/ticket/domain/dtos/CreateEventRequestDTO.java
@@ -2,6 +2,11 @@ package com.danny.ticket.domain.dtos;
 
 import com.danny.ticket.domain.CreateTicketTypeRequest;
 import com.danny.ticket.domain.entities.EventStatusEnum;
+import jakarta.validation.MessageInterpolator;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,13 +20,21 @@ import java.util.List;
 @NoArgsConstructor
 public class CreateEventRequestDTO {
 
+    @NotBlank(message = "Event name is required")
     private String name;
+
     private LocalDateTime start;
     private LocalDateTime end;
+
+    @NotBlank(message = "venue information is required")
     private String venue;
     private LocalDateTime salesStart;
     private LocalDateTime salesEnd;
+
+    @NotNull(message = "Event Status must be provided")
     private EventStatusEnum status;
-    private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
+    @NotEmpty(message = "Atleast one ticket type is required")
+    @Valid
+    private List<CreateTicketTypeRequestDTO> ticketTypes;
 
 }

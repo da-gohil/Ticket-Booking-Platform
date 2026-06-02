e6745006a735e989ed26aec4ac1ba84b1b5f87d1
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-02 12:17:25 -0400

    Create CreateTicketTypeRequestDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/CreateTicketTypeRequestDTO.java
@@ -0,0 +1,25 @@
+package com.danny.ticket.domain.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTicketTypeRequestDTO {
+
+    @NotBlank(message = "name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @PositiveOrZero(message = "Price must be zero or greater")
+    private Double price;
+
+    private String description;
+
+    private Integer totalAvailable;
+}

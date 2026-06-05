4a449cd8635c4e4b4d3a449898d086576e971a33
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-05 13:16:30 -0400

    Update EventService.java

--- src/main/java/com/danny/ticket/services/EventService.java
+++ src/main/java/com/danny/ticket/services/EventService.java
@@ -3,9 +3,18 @@ package com.danny.ticket.services;
 import com.danny.ticket.domain.CreateEventRequest;
 import com.danny.ticket.domain.entities.Event;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EventService {
+
     Event createEvent(UUID organizerId, CreateEventRequest event);
+
+    Page<Event> listEventForOrganizer(UUID organizerId, Pageable pageable);
+
+    Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
 
 }

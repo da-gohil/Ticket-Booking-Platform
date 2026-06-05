065ec77e3bb18d805eea6a28a0269517b9ca8d36
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-05 13:16:22 -0400

    Create ListEventResponseDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/ListEventResponseDTO.java
@@ -0,0 +1,36 @@
+package com.danny.ticket.domain.dtos;
+
+import com.danny.ticket.domain.entities.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListEventResponseDTO {
+
+    private UUID id;
+
+    private String name;
+
+
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    private String venue;
+
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+
+    private EventStatusEnum status;
+
+    private List<ListEventTicketTypeResponseDTO> ticketType = new ArrayList();
+
+}

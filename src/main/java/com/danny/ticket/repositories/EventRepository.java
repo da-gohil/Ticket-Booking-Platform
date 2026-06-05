7e9da8344cf67819674b41b6655940d147a19793
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-05 13:16:28 -0400

    Update EventRepository.java

--- src/main/java/com/danny/ticket/repositories/EventRepository.java
+++ src/main/java/com/danny/ticket/repositories/EventRepository.java
@@ -1,10 +1,17 @@
 package com.danny.ticket.repositories;
 
 import com.danny.ticket.domain.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
+
+    Optional<Event> findByIdAndOrganizerId(UUID id, UUID organizerId);
 
 }

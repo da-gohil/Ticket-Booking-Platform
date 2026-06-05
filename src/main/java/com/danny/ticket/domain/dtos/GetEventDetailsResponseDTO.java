cc6e58278420cca01c57743c36d585e414344a6f
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-05 13:16:20 -0400

    Create GetEventDetailsResponseDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/GetEventDetailsResponseDTO.java
@@ -0,0 +1,40 @@
+package com.danny.ticket.domain.dtos;
+
+import com.danny.ticket.domain.entities.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SecondaryRow;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetEventDetailsResponseDTO {
+
+    private UUID id;
+
+    private String name;
+
+    private LocalDateTime start;
+
+    private LocalDateTime end;
+
+    private String venue;
+
+    private LocalDateTime salesStart;
+
+    private LocalDateTime salesEnd;
+
+    private EventStatusEnum status;
+
+    private List<GetEventDetailsResponseDTO> ticketTypes = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}

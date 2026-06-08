d4f9ae2b3da0244ae7e4efb0e71a477f9448d0f5
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-08 17:06:44 -0400

    Create TicketTypeNotFoundException.java

---
+++ src/main/java/com/danny/ticket/exceptions/TicketTypeNotFoundException.java
@@ -0,0 +1,25 @@
+package com.danny.ticket.exceptions;
+
+public class TicketTypeNotFoundException extends EventTicketException {
+
+    public TicketTypeNotFoundException() {
+    }
+
+    public TicketTypeNotFoundException(String message) {
+        super(message);
+    }
+
+    public TicketTypeNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public TicketTypeNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TicketTypeNotFoundException(String message, Throwable cause,
+                                       boolean enableSuppression,
+                                       boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}

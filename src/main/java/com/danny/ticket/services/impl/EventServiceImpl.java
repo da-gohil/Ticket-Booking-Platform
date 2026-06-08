52c1cd00ae5bdab2f6b8cf01f22d0c5f3b8c343c
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-08 15:01:42 -0400

    Update EventServiceImpl.java

--- src/main/java/com/danny/ticket/services/impl/EventServiceImpl.java
+++ src/main/java/com/danny/ticket/services/impl/EventServiceImpl.java
@@ -9,9 +9,12 @@ import com.danny.ticket.repositories.EventRepository;
 import com.danny.ticket.repositories.UserRepository;
 import com.danny.ticket.services.EventService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -52,5 +55,15 @@ public class EventServiceImpl implements EventService {
         eventToCreate.setTicketTypes(ticketTypesToCreate);
 
         return eventRepository.save(eventToCreate);
+    }
+
+    @Override
+    public Page<Event> listEventForOrganizer(UUID organizerId, Pageable pageable) {
+        return eventRepository.findByOrganizerId(organizerId, pageable);
+    }
+
+    @Override
+    public Optional<Event> getEventForOrganizer(UUID organizerId, UUID id) {
+        return eventRepository.findByIdAndOrganizerId(id, organizerId);
     }
 }

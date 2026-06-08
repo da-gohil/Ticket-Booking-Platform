92fd5e47bca37985b1657359c4ddf233aab61dda
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-08 17:06:34 -0400

    Create GetEventTicketTypesResponseDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/GetEventTicketTypesResponseDTO.java
@@ -0,0 +1,27 @@
+package com.danny.ticket.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetEventTicketTypesResponseDTO {
+
+    private UUID id;
+    private String name;
+
+    private Double price;
+
+    private String description;
+
+    private Integer totalAvailable;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}

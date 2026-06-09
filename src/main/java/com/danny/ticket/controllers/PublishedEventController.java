1b4d8a5e7497327663916005115a8e7f4093cbf1
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-09 14:11:27 -0400

    Create PublishedEventController.java

---
+++ src/main/java/com/danny/ticket/controllers/PublishedEventController.java
@@ -0,0 +1,28 @@
+package com.danny.ticket.controllers;
+
+import com.danny.ticket.domain.dtos.ListPublishedEventResponseDTO;
+import com.danny.ticket.mappers.EventMapper;
+import com.danny.ticket.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/published-events")
+@RequiredArgsConstructor
+public class PublishedEventController {
+
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @GetMapping
+    public ResponseEntity<Page<ListPublishedEventResponseDTO>> listPublishedEvents(Pageable pageable){
+        return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
+                .map(eventMapper::toListPublishedEventResponseDTO)
+        );
+    }
+}

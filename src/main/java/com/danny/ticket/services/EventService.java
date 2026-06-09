175a7907702e2b83f5241222bed6a798b531bb26
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-09 14:11:19 -0400

    Update EventService.java

--- src/main/java/com/danny/ticket/services/EventService.java
+++ src/main/java/com/danny/ticket/services/EventService.java
@@ -21,4 +21,9 @@ public interface EventService {
     Event updateEventForOrganizer(UUID organizerId, UUID id, UpdateEventRequest event);
 
     void deleteEventForOrganizer(UUID organizerId, UUID id);
+
+    Page<Event> listPublishedEvents(Pageable pageable);
+
+    Page<Event> searchPublishedEvents(String query, Pageable pageable);
+
 }

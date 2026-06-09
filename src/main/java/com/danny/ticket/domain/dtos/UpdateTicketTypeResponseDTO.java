d6e85d3d8b2cdaddc39de453188bf73402b82eb1
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-09 11:25:07 -0400

    Create UpdateTicketTypeResponseDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/UpdateTicketTypeResponseDTO.java
@@ -0,0 +1,21 @@
+package com.danny.ticket.domain.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateTicketTypeResponseDTO {
+
+    private UUID uuid;
+    private String name;
+    private Double price;
+    private String description;
+    private Integer totalAvailable;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

4b8bfdcaae03d3f612028fd175f913fd0de1b5ae
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-09 11:25:04 -0400

    Create UpdateTicketTypeRequestDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/UpdateTicketTypeRequestDTO.java
@@ -0,0 +1,27 @@
+package com.danny.ticket.domain.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateTicketTypeRequestDTO {
+
+    private UUID id;
+
+    @NotBlank(message = "name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @PositiveOrZero(message = "Price must be zero or greater")
+    private Double price;
+    private String description;
+    private Integer totalAvailable;
+}

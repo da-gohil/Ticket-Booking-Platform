8478fcfdc63956fc07cfc4777f7744d35237d29e
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-10 11:23:07 -0400

    Create GetPublishedEventDetailsResponseDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/GetPublishedEventDetailsResponseDTO.java
@@ -0,0 +1,24 @@
+package com.danny.ticket.domain.dtos;
+
+import com.danny.ticket.domain.entities.EventStatusEnum;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPublishedEventDetailsResponseDTO {
+
+    private UUID id;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private List<GetPublishedEventDetailsTicketTypeResponseDTO> ticketTypes = new ArrayList<>();
+
+}

83f2df0f2f81b10c3f3c0742be871eeedfad4ade
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-17 17:24:42 -0400

    Update Ticket.java

--- src/main/java/com/danny/ticket/domain/entities/Ticket.java
+++ src/main/java/com/danny/ticket/domain/entities/Ticket.java
@@ -20,6 +20,7 @@ import java.util.UUID;
 @Table(name = "tickets")
 public class Ticket {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 

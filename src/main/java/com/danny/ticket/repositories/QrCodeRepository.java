261d37bb5b118d4142e63dafc6b429cd90a61cf5
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-17 17:25:02 -0400

    Update QrCodeRepository.java

--- src/main/java/com/danny/ticket/repositories/QrCodeRepository.java
+++ src/main/java/com/danny/ticket/repositories/QrCodeRepository.java
@@ -3,9 +3,11 @@ package com.danny.ticket.repositories;
 import com.danny.ticket.domain.entities.QrCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
-
+    Optional<QrCode> findByTicketIdAndTicketPurchaserId(UUID ticketId, UUID ticketPurchaseId);
 }

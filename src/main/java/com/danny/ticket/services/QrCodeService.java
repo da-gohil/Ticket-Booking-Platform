73a4976533a662355277f457ec74ca9d5c3fb5a4
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-17 17:25:32 -0400

    Update QrCodeService.java

--- src/main/java/com/danny/ticket/services/QrCodeService.java
+++ src/main/java/com/danny/ticket/services/QrCodeService.java
@@ -3,8 +3,11 @@ package com.danny.ticket.services;
 import com.danny.ticket.domain.entities.QrCode;
 import com.danny.ticket.domain.entities.Ticket;
 
+import java.util.UUID;
+
 public interface QrCodeService {
 
     QrCode generateQrCode(Ticket ticket);
-    
+
+    byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }

42e468861b1d1465643f14ef03dce083d5d450db
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-17 17:24:30 -0400

    Create GetTicketResponseDTO.java

---
+++ src/main/java/com/danny/ticket/domain/dtos/GetTicketResponseDTO.java
@@ -0,0 +1,27 @@
+package com.danny.ticket.domain.dtos;
+
+import com.danny.ticket.domain.entities.TicketStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetTicketResponseDTO {
+
+    private UUID id;
+    private TicketStatusEnum status;
+    private Double price;
+    private String description;
+
+    private String eventName;
+    private String eventVenue;
+
+    private LocalDateTime eventStart;
+    private LocalDateTime eventEnd;
+
+}

58b97f3ec7da3e9be32fcb1cfd9742bbc2a0e30f
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-06-17 17:25:35 -0400

    Create TicketService.java

---
+++ src/main/java/com/danny/ticket/services/TicketService.java
@@ -0,0 +1,16 @@
+package com.danny.ticket.services;
+
+import com.danny.ticket.domain.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TicketService {
+
+    Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable);
+
+    Optional<Ticket> getTicketForUser(UUID userId, UUID ticketId);
+
+}

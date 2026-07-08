44836886f096ba7a39024394dd01793997196fb8
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-07-08 00:16:44 -0400

    Update TicketService.java

--- src/main/java/com/danny/ticket/services/TicketService.java
+++ src/main/java/com/danny/ticket/services/TicketService.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 
 public interface TicketService {
 
+    Ticket purchaseTicket(UUID userId, UUID ticketTypeId);
+
     Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable);
 
     Optional<Ticket> getTicketForUser(UUID userId, UUID ticketId);

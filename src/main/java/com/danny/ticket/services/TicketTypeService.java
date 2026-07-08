058d82630fd225e6c7304909aa0160316fb492fd
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-07-08 00:16:40 -0400

    Delete TicketTypeService.java

--- src/main/java/com/danny/ticket/services/TicketTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.danny.ticket.services;
-
-import com.danny.ticket.domain.entities.Ticket;
-
-import java.util.UUID;
-
-public interface TicketTypeService {
-    Ticket purchaseTicket(UUID userId, UUID ticketTypeId);
-}
